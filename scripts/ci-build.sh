2c3a15d120529a9f3da37b3d15f24314443053f4
Bogdan Teodorescu <31422391+Atomique13@users.noreply.github.com>
2024-06-03 03:54:48 +0300

    Update ci-build.sh

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -69,14 +69,7 @@ start_test klippy "Test klippy import (Python3)"
 $PYTHON klippy/klippy.py --import-test
 finish_test klippy "Test klippy import (Python3)"
 
-start_test klippy "Test klippy import (Python2)"
-$PYTHON2 klippy/klippy.py --import-test
-finish_test klippy "Test klippy import (Python2)"
-
 start_test klippy "Test invoke klippy (Python3)"
 $PYTHON scripts/test_klippy.py -d ${DICTDIR} test/klippy/*.test
 finish_test klippy "Test invoke klippy (Python3)"
 
-start_test klippy "Test invoke klippy (Python2)"
-$PYTHON2 scripts/test_klippy.py -d ${DICTDIR} test/klippy/*.test
-finish_test klippy "Test invoke klippy (Python2)"
